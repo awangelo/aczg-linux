fd3a654fb4f810db65d4907e63ae0c6257b05259
awangelo <anggg.gaio@gmail.com>
2026-01-31 19:57:27 -0300

    feat: Task4

---
+++ config/aliases.sh
@@ -0,0 +1,4 @@
+alias aczgnew='~/.local/bin/aczg/aczg-init-project.sh'
+alias aczginit='~/.local/bin/aczg/aczg-branch-init.sh'
+alias aczgfinish='~/.local/bin/aczg/aczg-branch-finish.sh'
+alias aczglog='grep "\[ACZG-CI\]" ~/.local/log/aczg/ci.log | tail -50'
